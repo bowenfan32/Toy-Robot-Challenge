d6122f7ed4b7c7dd3138b62ffe25d89c8c824d85
Bowen Fan <bowenfan32@gmail.com>
2017-09-14 15:59:44 +1000

    Add robot move function

--- src/js/script.js
+++ src/js/script.js
@@ -6,13 +6,38 @@ var canvas = document.getElementById("canvas");
 var img = document.getElementById("img-robot");
 var context = canvas.getContext("2d");
 
+var dir = ["NORTH", "EAST", "SOUTH", "WEST"];
+
 
 function promptError(err) {
     error.innerHTML = err;
 }
 
 
+function move() {
+    switch(robot.dir) {
+        case "NORTH":
+            clearRobot();
+            robot = new Robot( robot.x , robot.y + 100, robot.dir);
+            drawRobot(robot);
+        case "EAST":
+            clearRobot();
+            robot = new Robot(robot.x + 100, robot.y, robot.dir);
+            drawRobot(robot);
+        case "SOUTH":
+            clearRobot();
+            robot = new Robot(robot.x, robot.y - 100, robot.dir);
+            drawRobot(robot);
+        case "WEST":
+            clearRobot();
+            robot = new Robot(robot.x - 100, robot.y, robot.dir);
+            drawRobot(robot);
+    }
+}
+
+
 function init() {
+    robot = new Robot(0 + 10, 0 + 10, "EAST");
     for (var x = 1; x < 601; x += 100) {
         context.moveTo(x, 1);
         context.lineTo(x, 501);
@@ -25,8 +50,7 @@ function init() {
 
     context.strokeStyle = "#000";
     context.stroke();
-
-    robot = new Robot(0, 0, "NORTH");
+    context.transform(1, 0, 0, -1, 0, canvas.height)
     drawRobot(robot);
 }
 
@@ -36,9 +60,17 @@ function Robot(x, y, dir) {
     this.dir = dir;
 }
 
-function drawRobot(newRobot) {
+function clearRobot() {
+    context.clearRect(robot.x , robot.y, 80, 80);
+}
 
-    context.drawImage(img, 10, 400 + 10, 80, 80);
+function drawRobot(newRobot) {
+    // context.scale(1, -1);
+// context.save();
+    // context.translate(50, 50);
+    // context.scale(1, -1);
+
+     context.drawImage(img, robot.x + 5, robot.y + 5, 80, 80);
 
 }
 
